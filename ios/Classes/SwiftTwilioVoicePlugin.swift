7919e0f408bc5c633ba20805c1044ded708e5256
Erchil66 <artecode66@gmail.com>
2022-12-08 12:53:02 +0800

    ios

--- ios/Classes/SwiftTwilioVoicePlugin.swift
+++ ios/Classes/SwiftTwilioVoicePlugin.swift
@@ -163,8 +163,8 @@ public class SwiftTwilioVoicePlugin: NSObject, FlutterPlugin,  FlutterStreamHand
             return;
         }else if flutterCall.method == "getParams"{
             
-            let haveData:String? = finaleSTL!.trimmingCharacters(in: .whitespaces);
-            ///
+            let haveData:String = finaleSTL!.trimmingCharacters(in: .whitespaces) ?? "";
+            //
             result(haveData)
             return;
         }
